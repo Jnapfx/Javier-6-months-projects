78e2b0f61e9d0e9b7a96cbe2ff4e9811e50ab1e2
Jnapfx <javiernpls@gmail.com>
2025-08-17 15:07:24 -0400

    troubleshooting github pages portfolio 4

--- docs/assets/js/dae-link.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var el = document.querySelector('.masthead .site-title, .site-header .site-title');
-  if (el) {
-    el.setAttribute('href', 'https://www.mydae.org/');
-    el.setAttribute('target', '_blank');
-    el.setAttribute('rel', 'noopener');
-  }
-});
